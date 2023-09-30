2b759a83f7092fb376a8c6c7dca31cc2de6ba0f9
brenoproti <moreirasgbr@gmail.com>
2023-09-30 13:26:50 -0300

    custom-http-request

---
+++ custom-http-request/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Accepts", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
